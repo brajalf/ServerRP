4c4647e6713e4ccb2e103f1a55edc2dd6ec24175
brajalf <brajalf@gmail.com>
2025-08-17 20:00:12 -0500

    Upload DJ

---
+++ myDj/config.lua
@@ -0,0 +1,33 @@
+Config = {}
+Translation = {}
+
+Translation = {
+    ['de'] = {
+        ['DJ_interact'] = 'Drücke ~g~E~s~, um auf das DJ Pult zuzugreifen',
+        ['title_does_not_exist'] = '~r~Dieser Titel existiert nicht!',
+    },
+
+    ['en'] = {
+        ['DJ_interact'] = 'Press ~g~E~s~, to mix',
+        ['title_does_not_exist'] = '~r~This song doesnt exist!',
+    }
+}
+
+Config.Locale = 'en'
+
+Config.useESX = true -- can not be disabled without changing the callbacks
+Config.enableCommand = false
+
+Config.enableMarker = true -- purple marker at the DJ stations
+
+Config.DJPositions = {
+    {
+        name = 'bahama',
+        pos = vector3(116.81999969482422, -1281.7900390625, 29.26000022888183),
+        requiredJob = nil, 
+        range = 25.0, 
+        volume = 1.0 --[[ do not touch the volume! --]]
+    }
+
+    --{name = 'bahama', pos = vector3(-1381.01, -616.17, 31.5), requiredJob = 'DJ', range = 25.0}
+}
