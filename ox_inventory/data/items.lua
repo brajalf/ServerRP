17712923bbee1fbf81a6860790d056db714354ed
brajalf <111999245+brajalf@users.noreply.github.com>
2025-08-25 01:09:08 -0500

    chore: remove cash icon

--- ox_inventory/data/items.lua
+++ ox_inventory/data/items.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+    cash = {
+        name = 'cash',
+        label = 'Efectivo',
+        weight = 0,
+        type = 'item',
+        image = 'cash.png',
+        unique = false,
+        useable = false,
+        description = 'Dinero en efectivo (visual)',
+    },
+}
