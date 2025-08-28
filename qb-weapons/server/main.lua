d70b81b163ea67a1943a685b4e5714ef4ed87fef
brajalf <brajalf@gmail.com>
2025-08-28 10:14:50 -0500

    Delete

--- qb-weapons/server/main.lua
+++
@@ -1,9 +0,0 @@
-local QBCore = exports['qb-core']:GetCoreObject()
-
--- Register ammo items as usable and trigger client reload
-for ammoItem, _ in pairs(Config.AmmoTypes or {}) do
-    QBCore.Functions.CreateUseableItem(ammoItem, function(source, item)
-        TriggerClientEvent('inventory:client:UseAmmo', source)
-    end)
-end
-
